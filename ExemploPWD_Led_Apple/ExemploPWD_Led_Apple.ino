54442e188f79c71786274987aedca25c3065f95f
Luiz Brandão <luizao.cmd@gmail.com>
2013-06-30 21:23:43 -0400

    Adicionando README aos Sketchbook

--- ExemploPWD_Led_Apple/ExemploPWD_Led_Apple.ino
+++ ExemploPWD_Led_Apple/ExemploPWD_Led_Apple.ino
@@ -1,15 +1,17 @@
-#define LED 9
-int i = 0;
+#define LED 9 // Defino o pino para qual iremos fornecer energia ao LED
+int i = 0; // variavel auxiliar
 
 void setup(){
-  pinMode(LED, OUTPUT);
+  pinMode(LED, OUTPUT); // Registro o LED
 }
 
 void loop(){
+  // for para ligar o LED
   for(i = 0; i < 255; i++){
     analogWrite(LED,i);
     delay(10);
   }
+  // for para desligar o LED
   for(i = 255; i > 0; i--){
     analogWrite(LED,i);
     delay(10);
